d93bec559f8e226b7239060e22196aa9636c37b6
Dan Comand <dcomand@gmail.com>
2015-04-22 21:36:17 -0400

    creating Listing model in database.

---
+++ db/migrate/20150423013455_create_listings.rb
@@ -0,0 +1,19 @@
+class CreateListings < ActiveRecord::Migration
+  def change
+    create_table :listings do |t|
+      t.string :address
+      t.string :bedrooms
+      t.string :bathrooms
+      t.string :price
+      t.string :sqft
+      t.text :description
+      t.string :agent
+      t.string :website
+      t.string :email
+      t.string :number
+      t.string :url
+
+      t.timestamps null: false
+    end
+  end
+end

4e5e140ffc48f39e1f1a37b67c75cf4bb5b522e9
Dan Comand <dcomand@gmail.com>
2015-04-22 23:00:41 -0400

    refactoring to save new pictures in databases.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,12 +8,24 @@ class ListingsController < ApplicationController
 	end
 
 	def new
+		@listing = Listing.new
 	end
 
-	def create
+	 def create
+    # make a new picture with what picture_params returns (which is a method we're calling)
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      # if the save for the picture was successful, go to index.html.erb
+      redirect_to listings_url
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+      render :new
+    end
+  end
 
-		render :text => "Saving a listing. Address: #{params[:address]}, Agent: #{params[:agent]}, URL: #{params[:url]}"
-	end
-
+  private
+  def listing_params
+    params.require(:listing).permit(:address, :agent, :url)
+  end
 
 end
